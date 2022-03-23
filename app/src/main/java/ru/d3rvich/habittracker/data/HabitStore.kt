5bcdb5d0a639fd9cff91ddb195b6807e1f494bdc
D3rvich <ilya24.1999@gmail.com>
2022-03-23 16:51:22 +0500

    Заменил спиннер на Dropdown menu для выбора метода сортировки

--- app/src/main/java/ru/d3rvich/habittracker/data/HabitStore.kt
+++ app/src/main/java/ru/d3rvich/habittracker/data/HabitStore.kt
@@ -2,34 +2,38 @@ package ru.d3rvich.habittracker.data
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import ru.d3rvich.habittracker.entity.HabitEntity
+import ru.d3rvich.habittracker.entity.HabitType
+import java.util.*
 
 object HabitStore {
     private val habits: MutableList<HabitEntity> = mutableListOf()
     private val habitsFlow = MutableStateFlow<List<HabitEntity>>(emptyList())
 
-//    init {
-//        runBlocking {
-//            launch {
-//                for (i in 0 until 10) {
-//                    val habit = HabitEntity(
-//                        id = UUID.randomUUID().toString(),
-//                        title = "Title $i",
-//                        description = "Description",
-//                        type = if (i % 2 == 0) HabitType.Good else HabitType.Bad,
-//                        count = 0,
-//                        frequency = 0,
-//                        priority = 0,
-//                        color = (0xFF00FF00).toInt(),
-//                        date = 0L,
-//                        doneDates = emptyList()
-//                    )
-//                    habits.add(habit)
-//                }
-//                habitsFlow.emit(habits.toList())
-//            }
-//        }
-//    }
+    init {
+        runBlocking {
+            launch {
+                for (i in 0 until 10) {
+                    val habit = HabitEntity(
+                        id = UUID.randomUUID().toString(),
+                        title = "Title $i",
+                        description = "Description",
+                        type = if (i % 2 == 0) HabitType.Good else HabitType.Bad,
+                        count = 0,
+                        frequency = 0,
+                        priority = i % 3,
+                        color = (0xFF00FF00).toInt(),
+                        date = (i * 1000 + i * i).toLong(),
+                        doneDates = emptyList()
+                    )
+                    habits.add(habit)
+                }
+                habitsFlow.emit(habits.toList())
+            }
+        }
+    }
 
     fun getHabits(): Flow<List<HabitEntity>> {
         return habitsFlow.asSharedFlow()
--- app/src/main/java/ru/d3rvich/habittracker/screens/habit_list/view/FilterFragment.kt
+++ app/src/main/java/ru/d3rvich/habittracker/screens/habit_list/view/FilterFragment.kt
@@ -5,8 +5,8 @@ import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -39,34 +39,29 @@ class FilterFragment : Fragment() {
                 viewModel.obtainEvent(HabitListEvent.OnFilterChange(text.toString()))
             }
         }
-        configureSortingSelector()
+        configureSortingSelector(viewModel.currentState.filterConfig.sortingEngine as HabitSortingVariants)
         configureSortingDirectionSelection()
     }
 
-    private fun configureSortingSelector() {
+    private fun configureSortingSelector(initialSortingMethod: HabitSortingVariants) {
         val sortingList = arrayOf(R.string.no_sorting to HabitSortingVariants.None,
             R.string.by_title to HabitSortingVariants.ByTitle,
             R.string.by_priority to HabitSortingVariants.ByPriority,
             R.string.by_creation_time to HabitSortingVariants.ByCreatedTime).map { getString(it.first) to it.second }
         val adapter =
             ArrayAdapter(requireContext(), R.layout.list_item, sortingList.map { it.first })
-        with(binding.sortingSelector) {
-            this.adapter = adapter
-            onItemSelectedListener =
-                object : AdapterView.OnItemSelectedListener {
-                    override fun onItemSelected(
-                        parentView: AdapterView<*>?,
-                        selectedItemView: View?,
-                        position: Int,
-                        id: Long,
-                    ) {
-                        val selectedItem = sortingList[position]
-                        viewModel.obtainEvent(HabitListEvent.OnSortingMethodChange(selectedItem.second))
-                    }
-
-                    override fun onNothingSelected(parentView: AdapterView<*>?) {
-                    }
+        with(binding.sortingSelector.editText as? AutoCompleteTextView) {
+            this?.let {
+                val selectedItemText = sortingList.find { it.second == initialSortingMethod }
+                selectedItemText?.let {
+                    setText(selectedItemText.first)
                 }
+                setAdapter(adapter)
+                setOnItemClickListener { _, _, index, _ ->
+                    val selectedItem = sortingList[index]
+                    viewModel.obtainEvent(HabitListEvent.OnSortingMethodChange(selectedItem.second))
+                }
+            }
         }
     }
 
@@ -82,14 +77,12 @@ class FilterFragment : Fragment() {
         binding.sortingDirectionGroup.addOnButtonCheckedListener { _, checkedId, isChecked ->
             when (checkedId) {
                 R.id.sorting_ascending_button -> {
-                    binding.sortingAscendingButton.isChecked = isChecked
-                    if (binding.sortingAscendingButton.isChecked) {
+                    if (isChecked) {
                         viewModel.obtainEvent(HabitListEvent.OnSortDirectionChange(SortDirection.ByAscending))
                     }
                 }
                 R.id.sorting_descending_button -> {
-                    binding.sortingDescendingButton.isChecked = isChecked
-                    if (binding.sortingDescendingButton.isChecked) {
+                    if (isChecked) {
                         viewModel.obtainEvent(HabitListEvent.OnSortDirectionChange(SortDirection.ByDescending))
                     }
                 }
